cd97b84e2deaac8f107813d242d5fad0e8967d9f
aisav <aisavikin@gmail.com>
2025-12-24 10:15:47 +0300

    debug=False

--- englishEasy/settings.py
+++ englishEasy/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-change-me-in-production'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['englisheasy.pythonanywhere.com', 'localhost']
 
 INSTALLED_APPS = [
